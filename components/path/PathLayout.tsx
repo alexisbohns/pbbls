9a1c0d818adf17cfe9cb151e029515f73f701c28
Bohns <hello@bohns.design>
2026-04-06 16:04:56 +0200

    enlarge main thread column and get rid of the grids

--- components/path/PathLayout.tsx
+++ components/path/PathLayout.tsx
@@ -9,17 +9,17 @@ type PathLayoutProps = {
 export function PathLayout({ children }: PathLayoutProps) {
   return (
     <>
-      <div className="flex-column items-start md:grid md:grid-cols-[1fr_380px_1fr] gap-6 px-4 md:px-6 pt-8 md:pt-8">
-        <aside className="flex justify-end sticky top-8">
+      <div className="flex-column items-start md:flex gap-6 px-4 md:px-6 pt-8 md:pt-8">
+        <aside className="flex justify-end sticky top-8 w-full">
           <div className="w-full md:max-w-[200px]">
             <PathProfileCard />
           </div>
         </aside>
 
-        <div className="min-w-0 mt-6 md:mt-0">
+        <div className="min-w-0 mt-6 md:mt-0 w-full md:min-w-[380px] md:max-w-[600px]">
           {children}
         </div>
-        <div aria-hidden className="hidden md:flex" />
+        <div aria-hidden className="hidden md:flex w-full" />
       </div>
     </>
   )
