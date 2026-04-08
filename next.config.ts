e597a937e8d5d82aa8e0ac2be2ae5239b26caf5b
Bohns <hello@bohns.design>
2026-04-08 21:25:38 +0200

    add local dev certificate

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import { withSerwist } from "@serwist/turbopack";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ['192.168.1.165'],
+};
 
 export default withSerwist(nextConfig);
-
-module.exports = {
-  allowedDevOrigins: ['192.168.1.165'],
-}
